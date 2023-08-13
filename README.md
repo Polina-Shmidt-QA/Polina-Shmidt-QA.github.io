b821a891faaa65d2d84b237ebc1f5dcccb05c04e
Polina Shmidt <120649861+Polina-Shmidt-QA@users.noreply.github.com>
2023-08-13 16:04:46 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Polina-Shmidt-QA.github.io
+# ShmidtQA
 Сайт-визитка на HTML+CSS+JS
+
+[![Header](https://github.com/Polina-Shmidt-QA/Polina-Shmidt-QA.github.io/blob/main/assets/logo.png)](https://polina-shmidt-qa.github.io//)
---
+++ js.js
@@ -0,0 +1,13 @@
+var button1 = document.getElementById('button1');
+button1.addEventListener('click', function() {
+	swal("Знаю:", "• HTTP, REST, SOAP, JSON, XML\n • Devtools, Charles\n • Postman\n • Qase.io, Miro\n • HTML, CSS, JS\n • Git\n • PostgreSQL, MySQL\n • Agile, Scrum, Kanban\n • JavaScript\n • WordPress\n • PhotoShop, Figma, Tilda\n ...\n Это не предел!)", {
+		button: "Sounds Good",
+	  });
+})
+
+var button2 = document.getElementById('button2');
+button2.addEventListener('click', function() {
+	swal("Умею:", "• быть милой и доброй\n • артистичной и дружелюбной\n • целеустремленной и активной\n ...\n Готова к коммуникации и общению!)", {
+		button: "Well Done",
+	  });
+})
